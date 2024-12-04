d0e8666000f9aa0821970c75b0b827ed121850d7
ChristianScarabelli <scachmabol@hotmail.it>
2024-12-04 17:13:02 +0100

    FIX: tags

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -1,18 +1,20 @@
-import style from './Tags.module.css'
+import style from './Tags.module.css';
 
 export default function Tags({ tags = [] }) {
     return (
         tags.length > 0 && ( // Mostro i tag solo se esistono
             <div className={style.tags_container}>
-                {tags.map((tag, index) => ( // genero i tag dinamicamente e aggiornabili tramite l'indice
-                    <span
-                        key={index}
-                        className={`${style.tag} ${style[`tag_${tag}`]}`} // con le [] leggo la propiretà dell'oggetto e costruisco con il `il nome del tag
-                    >
-                        {tag}
-                    </span>
-                ))}
+                {tags.map((tag, index) => {      // Apro un blocco di codice nella callback
+                    const formattedTag = tag.toLowerCase().replaceAll(' ', '_')      // Formattazione del tag
+                    return (
+                        <span
+                            key={index}
+                            className={`${style.tag} ${style[`tag_${formattedTag}`]}`}    // Classe dinamica basata sul tag formattato
+                        >
+                            {tag}
+                        </span>
+                    )
+                })}
             </div>
-        )
-    )
+        ))
 }
